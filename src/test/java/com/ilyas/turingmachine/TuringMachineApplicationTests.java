0c02afd19796088636cd2379c9961ca9511155c9
AnandaIlyasa <anandailyasa23244@student.uns.ac.id>
2022-07-01 14:06:47 +0700

    fixing test

--- src/test/java/com/ilyas/turingmachine/TuringMachineApplicationTests.java
+++ src/test/java/com/ilyas/turingmachine/TuringMachineApplicationTests.java
@@ -1,9 +1,5 @@
 package com.ilyas.turingmachine;
 
-import java.util.stream.IntStream;
-
-import com.ilyas.turingmachine.TM.TuringMachine;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,20 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 class TuringMachineApplicationTests {
 
 	@Autowired
-	private TuringMachine additionTM;
-	@Autowired
 	private TuringMachineController controller;
 
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	void testIfTM_tapeMatch() {
-		String input = "000c11111";
-		additionTM.setTape(input);
-		IntStream.range(0, input.length())
-				.forEach(i -> Assertions.assertEquals(input.substring(i, i + 1), additionTM.getTape().get(i)));
 	}
 
 	@Test
